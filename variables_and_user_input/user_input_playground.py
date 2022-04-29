6cb62778fc0fcd9ee68aadf9c40abbef2a878517
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-04-30 09:25:07 +1000

    python :,)

---
+++ variables_and_user_input/user_input_playground.py
@@ -0,0 +1,6 @@
+# # Input func always
+# name = input("What is your name?")
+# print(name)
+
+number = int(input("Enter a number"))
+print(int(number) + 5)
---
+++ variables_and_user_input/variables_playground.py
@@ -0,0 +1,56 @@
+weather = "rainy"
+name = "Sarah"
+
+# print(weather)
+# print("rainy")
+
+# Data Types - string is a text data
+# Integer numerical data (whole number)
+# Float numerical (with decimals)
+
+height = 155
+weight = 55.8
+
+# print(type(name))
+# print(type(height))
+# print(type(weight))
+
+day = "Saturday"
+# print(type(day))
+message = f"Today is {day}!"
+message2 = "Today is" + day
+# print(message)
+
+# Integers
+# Run distance in m
+run1_dist = 1400
+run2_dist = 1800
+
+# addition
+total_dist = run1_dist + run2_dist
+# print(total_dist)
+
+# Float
+# Run distance in km
+run3_dist = 1.7
+run4_dist = 1.35
+# print(total_dist)
+
+# Addition2
+total_dist = run3_dist + run4_dist
+# print(total_dist)
+
+#Division and Multiplication
+print(run1_dist / 1000)
+print(run3_dist * 1000)
+
+# Division always produces float
+# Other calculations depends on the date type
+# As long as there is one float, float
+
+# Different scenarios
+name = "Sarah"
+# print(name + run2_dist)
+# print(name * run2_dist)
+# print(name + str(run1_dist))
+print(name * int(run4_dist))
