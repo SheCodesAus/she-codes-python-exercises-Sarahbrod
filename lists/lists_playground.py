66a70ac870e0eb2dedf7419932a7f266719da8e0
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-04-30 13:00:33 +1000

    pythonnnnn

--- lists/lists_playground.py
+++ lists/lists_playground.py
@@ -1,2 +1,67 @@
 # Lists
-['a', 'b', 'c']
+# 0 elements
+# 0 index, 0,1,2
+
+# characters = ["a", "b", "c"]
+# print(characters)
+# if "d" in characters:
+#     print("Good")
+# else:
+#     characters.append("d")
+#     print(characters)
+
+# print(characters.remove("a"))
+# print(characters)
+
+# print(characters)
+# print(type(characters))
+# print(characters[0])
+# characters.append("d")
+# characters.extend(["e", "f"])
+# characters.insert(1, "g")
+# print(characters[-2])
+# print(characters[0:2])  # starting index included, stopping index
+# print(characters[1:3])
+
+asli = [[3, 5]], ["blue", "pink"]]
+print(asli[0])
+
+chilli_wishlist= ["igloo", "chicken", "donut toy", "cardboard box"]
+
+if "blueberries" in chilli_wishlist:
+    print("blueberries")
+else:
+    chilli_wishlist.append("blueberries")
+    print(chilli_wishlist)
+
+# print(len(chilli_wishlist))
+# if len(chilli_wishlist) > 8:
+#     print("Chilli wants a lot of stuff!")
+
+    # if "tennis ball" in chilli_wishlist:
+    #     print("chilli would like a tennis ball.")
+    # else:
+    #     print("chilli doesnt want a tennis ball.")
+
+    # print(chilli_wishlist)
+    # print(chilli_wishlist[2])
+    # print(chilli_wishlist[-1])
+    # print(chilli_wishlist[3])
+    # print(chilli_wishlist[1:3])
+    # chilli_wishlist[1] = "carrot" - how to edit and override elements in the list
+    # print(chilli_wishlist)
+
+    # chilli_wishlist = ["igloo", "chicken", "donut toy", "cardboard box"]
+    # # Print the sublist of items positions 2 through to 3
+    # # # Print the item in the -3 position
+
+    # # print(chilli_wishlist[2:4])
+    # # print(chilli_wishlist[-3])
+
+    # chilli_wishlist = ["igloo", "chicken", "donut toy", "cardboard box"]
+    # chilli_wishlist[2] = "bowl"
+    # chilli_wishlist.append("dog mat")
+    # print(chilli_wishlist.extend(["kong", "tennis ball", "crocodile toy"]))
+    # chilli_wishlist.pop(2)
+    # chilli_wishlist.remove("igloo")
+    # print(chilli_wishlist)
